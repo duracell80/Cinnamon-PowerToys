c39d380d628cc5db20a630ed2dc3598449b5f70f
Lee Jordan <duracell80@users.noreply.github.com>
2022-09-22 00:11:57 -0500

    Hydrogen Theme

--- sounds/convert.sh
+++ sounds/convert.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 mkdir -p ./ogg
-for file in *.ogg; do
+for file in *.oga; do
     filebasename=`basename "$file" .ogg`
-    fileout="./ogg/$filebasename.ogg"
+    fileout="./ogg/${filebasename/.oga/""}.ogg"
 
     ffmpeg -i "$file" -acodec libvorbis -ab 32k -ab 64k -ar 44100 "$fileout"
 done
---
+++ sounds/hydrogen/readme.txt
@@ -0,0 +1,4 @@
+Based on Oxygen by @git:kuroehanako 
+https://www.gnome-look.org/p/1556448 
+
+with cinnamon desktop focused enhancements from Notification Sounds https://notificationsounds.com/notification-sounds
