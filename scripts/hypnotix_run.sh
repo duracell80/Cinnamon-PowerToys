2ceb540cc4830afec4a73b391a4e01d4bd8c504d
Lee Jordan <duracell80@users.noreply.github.com>
2022-10-23 22:56:35 -0500

    Tweaks

---
+++ scripts/hypnotix_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+MSG_0=0
+
+LWD="$HOME/.local/share/powertoys"
+CWD="$HOME/.cache/powertoys"
+HWD="$HOME/.cache/hypnotix"
+
+$LWD/hypnotix_yt.sh
+
+$LWD/hdhr_channels.py
+$LWD/hypnotix_hdhr.sh
+
+hypnotix
