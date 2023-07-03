fbd51e59cfa4ce2f4e0fb62565205e0a14e5ef00
Lee Jordan <duracell80@users.noreply.github.com>
2023-07-03 10:55:19 -0500

    Update install-hdhomerun.sh

--- install-hdhomerun.sh
+++ install-hdhomerun.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-sudo apt install -y git
+if ! [ -x "$(which hdhomerun_config)" ]; then
+  sudo apt install -y git automake autoconf
 
-#if wget -q --method=HEAD http://hdhomerun.local; then
     CWD=$(pwd)
     LWD=$HOME/.local/share/powertoys
     LBD=$HOME/.local/bin
 
     mkdir -p $CWD/deps
 
-    cd $CWD/deps 
+    cd $CWD/deps
     git clone https://github.com/Silicondust/libhdhomerun.git
     cd $CWD/deps/libhdhomerun
+    autoconf
     ./configure
     make
     sudo make install
     cp $CWD/deps/libhdhomerun/hdhomerun_config $LBD
     cd $CWD
+
+  exit 1
+fi
+
+
+~/.local/share/powertoys/hypnotix_hdhr.sh
+
+
+#if wget -q --method=HEAD http://hdhomerun.local; then
+#    echo "[i] Found HDHomeRun Tuners on this network"
+#    echo "[i] Visit http://hdhomerun.local in a browser"
 #else
 #    echo "[i] There are no HDHomeRun tuners on this network"
 #    echo "[i] Visit https://info.hdhomerun.com for more details"
