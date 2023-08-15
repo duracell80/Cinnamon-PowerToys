b5384a5fdb0a4061a624de90d14ebb535e6a21c8
Lee Jordan <duracell80@users.noreply.github.com>
2023-08-14 23:17:36 -0500

    Add Breeze Cursors

--- install-cursors.sh
+++ install-cursors.sh
@@ -25,13 +25,32 @@ cd $HOME/git/XCursor-pro/bin
 tar -xvf XCursor-Pro-Dark.tar.gz
 tar -xvf XCursor-Pro-Light.tar.gz
 
-#cp -r XCursor-Pro-Dark $HOME/.icons
-#cp -r XCursor-Pro-Light $HOME/.icons
-
 sudo cp -r XCursor-Pro-Dark /usr/share/icons
 sudo cp -r XCursor-Pro-Light /usr/share/icons
 
+
+if [ ! -d "$HOME/git/BreezeX_Cursor" ]; then
+        mkdir -p $HOME/git && cd $HOME/git
+        git clone https://github.com/ful1e5/BreezeX_Cursor.git
+fi
+
+
+cd $HOME/git/BreezeX_Cursor
+./release.sh
+
+cd $HOME/git/BreezeX_Cursor/bin
+tar -xvf BreezeX-Light.tar.gz
+tar -xvf BreezeX-Dark.tar.gz
+
+sudo cp -r BreezeX-Dark /usr/share/icons
+sudo cp -r BreezeX-Light /usr/share/icons
+
+
 gsettings set org.cinnamon.desktop.interface cursor-theme 'XCursor-Pro-Light'
 gsettings set org.gnome.desktop.interface cursor-theme 'XCursor-Pro-Light'
+gsettings set x.dm.slick-greeter cursor-theme-name 'XCursor-Pro-Light'
+
+gsettings set x.dm.slick-greeter icon-theme-name 'Minty-Grey'
+gsettings set x.dm.slick-greeter theme-name 'Minty-Grey'
 
 sudo update-alternatives --config x-cursor-theme
