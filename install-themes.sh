743ee3dfce8a5f37c2992f52b1ccf3ea4688bed3
Lee Jordan <duracell80@users.noreply.github.com>
2023-08-18 18:17:21 -0500

    Update theme

--- install-themes.sh
+++ install-themes.sh
@@ -6,3 +6,8 @@ LBD=$HOME/.local/bin
 LTD=$HOME/.themes
 
 sudo apt remove mint-l-theme
+sudo apt install sassc inkscape optipng
+
+if [ ! -d "$HOME/git/mint-themes" ]; then
+        git clone https://github.com/linuxmint/mint-themes.git
+fi
