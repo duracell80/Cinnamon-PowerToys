4f89352a43484fede0e1058b0239ebe5cdf61573
Lee Jordan <duracell80@users.noreply.github.com>
2023-09-03 12:34:46 -0500

    Update app install script

--- install-apps.sh
+++ install-apps.sh
@@ -4,11 +4,12 @@ CWD=$(pwd)
 LWD=$HOME/.local/share/powertoys
 LBD=$HOME/.local/bin
 
+sudo apt update
+
 if ! [ -x "$(which cinnamon)" ]; then
 	echo "[i] Cinnamon Desktop Not Found"
 	exit
 else
-	sudo apt update
 	sudo apt install -y wget git
 	mkdir -p $CWD/apps
 	cd $CWD/apps
@@ -19,13 +20,13 @@ fi
 gsettings set com.linuxmint.updates auto-update-cinnamon-spices false
 #cinnamon-spice-updater --update-all
 
-cd $CWD
-# Unsplash Wallpapers
-cd $HOME/Downloads
-wget -nc "https://github.com/soroushchehresa/unsplash-wallpapers/releases/download/v1.3.0/unsplash-wallpapers_1.3.0_amd64.deb"
-sudo dpkg -i $HOME/Downloads/unsplash-wallpapers_1.3.0_amd64.deb
+if ! [ -x "$(which unsplash-wallpapers)" ]; then
+	cd $HOME/Downloads
+	wget -nc "https://github.com/soroushchehresa/unsplash-wallpapers/releases/download/v1.3.0/unsplash-wallpapers_1.3.0_amd64.deb"
+	sudo dpkg -i $HOME/Downloads/unsplash-wallpapers_1.3.0_amd64.deb
 
-cd $CWD
+	cd $CWD
+fi
 
 cinnamon-install-spice applet $CWD/apps/cinnamon-spices-applets/radio@driglu4it
 cinnamon-install-spice applet $CWD/apps/cinnamon-spices-applets/weather@mockturtl
@@ -52,5 +53,50 @@ else
 fi
 
 
+# PPAs
 
-cinnamon-settings applets
+if ! [ -x "$(which obs)" ]; then
+
+        read -p "[Q] Install OBS Studio from PPA (y/n)? " answer
+	case ${answer:0:1} in
+    		y|Y )
+        		sudo add-apt-repository ppa:obsproject/obs-studio
+                	sudo apt update
+                	sudo apt -y install obs-studio
+    		;;
+    		* )
+        		sudo add-apt-repository --remove ppa:obsproject/obs-studio
+			sudo apt update
+			echo "[i] OBS PPA not added"
+    		;;
+	esac
+
+
+	if ! [ -x "$(which obs)" ]; then
+		read -p "[Q] Install OBS Studio from APT (y/n)? " answer
+        	case ${answer:0:1} in
+                	y|Y )
+                        	sudo add-apt-repository --remove ppa:obsproject/obs-studio
+                        	sudo apt update
+                        	sudo apt -y install obs-studio
+                	;;
+                	* )
+                        	echo "[i] OBS APT not added"
+                ;;
+        	esac
+
+	fi
+fi
+
+# Flatpak
+
+if [ -x "$(which flatpak)" ]; then
+	echo "[i] Flatpak is available on this system"
+
+	HAVE_MC=$(flatpak list | grep -i "Mission Center" | wc -l)
+	if [[ "$HAVE_MC" == "0" ]]; then
+		flatpak install io.missioncenter.MissionCenter
+	fi
+fi
+
+#cinnamon-settings applets
