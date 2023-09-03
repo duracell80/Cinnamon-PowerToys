722c8f85b643e9f05731d3a84057ee898b8307b4
Lee Jordan <duracell80@users.noreply.github.com>
2023-09-03 11:27:27 -0500

    Reduce file writes in screensaver watch

--- scripts/watch_screensaver.sh
+++ scripts/watch_screensaver.sh
@@ -8,12 +8,14 @@ SFILE_LK="${HOME}/.local/state/screensaver/bg_lock.txt"
 if [ -x "$(which cinnamon-screensaver)" ]; then
 	while true
 	do
-		cinnamon-screensaver-command -q > $STATE_TX
 		if [ ! -f $FILE_LK ]; then
 			echo $(gsettings get x.dm.slick-greeter background) > $HOME/.local/state/screensaver/bg_lock.txt
 		fi
-
-		STATE_SS=$(cat $STATE_TX)
+        
+        #cinnamon-screensaver-command -q > $STATE_TX
+		#STATE_SS=$(cat $STATE_TX)
+        
+        STATE_SS=$(cinnamon-screensaver-command -q)
 		if [[ $STATE_SS == *"inactive"* ]]; then
 			STATE_RS=$(cat $SFILE_BG)
 			STATE_LK=$(cat $SFILE_LK)
