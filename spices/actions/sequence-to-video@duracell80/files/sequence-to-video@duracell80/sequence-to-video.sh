d338af29f6e96685c4ba630956733a7aa208db5e
Lee Jordan <duracell80@users.noreply.github.com>
2024-01-23 23:57:46 -0600

    Add more sort options

--- spices/actions/sequence-to-video@duracell80/files/sequence-to-video@duracell80/sequence-to-video.sh
+++ spices/actions/sequence-to-video@duracell80/files/sequence-to-video@duracell80/sequence-to-video.sh
@@ -7,7 +7,6 @@
 DIR_PWD=$(pwd)
 DIR_TGT="${1%/*}/"
 
-
 # ZENITY DOESN'T SEEM TO WORK WITHOUT THIS WHEN NOT USING ENGLISH
 LANG=$(locale | grep LANGUAGE | cut -d= -f2 | cut -d_ -f1)
 REGI=$(locale | grep LANGUAGE | cut -d= -f2)
@@ -42,9 +41,9 @@ if [[ $(compgen -c | grep -iw 'ffmpeg' | head -n1 | wc -l) == "0" ]]; then
 else
    
     TIS=$(date +%s)
-    TIC=$(date +%c | sed 's/:/-/g' | sed 's/ /_/g')
+    TIC=$(date +%c | sed 's/:/-/g' | sed 's/ /-/g')
 
-    touch "${DIR_TGT}/ffseq_${TIS}.txt"
+    touch "${DIR_TGT}/ffseq-${TIS}.txt"
     
 
     #ASK FOR A SORTING ORDER
@@ -57,27 +56,59 @@ else
 	        "02" "Filename descending" \
             "03" "Filename unique (ascending)" \
             "04" "Filename unique (descending)" \
-            "05" "Filename random" )
+            "05" "Filename random" \
+            "06" "File modified (ascending)" \
+            "07" "File modified (descending)" )
 
     if [ "$SORT" = "01" ]; then
         # LOWER TO HIGHER
         SORTED=($(sort -f <(printf "%s\n" "$@")))
+        MARKER="ascending"
 
     elif [ "$SORT" = "02" ]; then
         # HIGHER TO LOWER
         SORTED=($(sort -fr <(printf "%s\n" "$@")))
+        MARKER="descending"
 
     elif [ "$SORT" = "03" ]; then
         # UNIQUE ASC        
         SORTED=($(sort -fu <(printf "%s\n" "$@")))
+        MARKER="unique-ascending"
 
     elif [ "$SORT" = "04" ]; then
         # UNIQUE DESC    
         SORTED=($(sort -fur <(printf "%s\n" "$@")))
+        MARKER="unique-descending"
 
     elif [ "$SORT" = "05" ]; then
         # RANDOM
         SORTED=($(sort -fR <(printf "%s\n" "$@")))
+        MARKER="random"
+
+    elif [ "$SORT" = "06" ] || [ "$SORT" = "07" ]; then
+        # FILE MODIFIED DATE
+        INFILE=($(printf "%s\n" "$@"))
+        BUCKET=()
+
+        for f in "${INFILE[@]}"; do
+            FILE_MOD=$(date -r "${f}" +%s)
+            BUCKET+=("${FILE_MOD}:${f}")
+        done
+
+            if [ "$SORT" = "06" ]; then
+                # ASC            
+                SORTED=($(sort <(printf "%s\n" "${BUCKET[@]}")))
+                MARKER="modified-ascending"
+            elif [ "$SORT" = "07" ]; then
+                # DESC
+                SORTED=($(sort -r <(printf "%s\n" "${BUCKET[@]}")))
+                MARKER="modified-descending"      
+            fi
+
+        for s in "${SORTED[@]}"; do
+            IFS=':' read -ra f <<< "$s"    
+            echo "file '${f[1]}'" >> "${DIR_TGT}/ffseq-${TIS}.txt"
+        done
 
     else
         # USER HITS CANCEL            
@@ -85,10 +116,14 @@ else
     fi
 
     for f in "${SORTED[@]}"; do
-        echo "file '${f}'" >> "${DIR_TGT}/ffseq_${TIS}.txt"
+        echo "file '${f}'" >> "${DIR_TGT}/ffseq-${TIS}.txt"
     done
 
-    ffmpeg -r 30 -f concat -safe 0 -i "${DIR_TGT}/ffseq_${TIS}.txt" -c:v libx264 -pix_fmt yuv420p "${DIR_TGT}/sequence_${TIC,,}.mp4"   
-    celluloid ${DIR_TGT}/sequence_${TIC,,}.mp4
-    rm -f "${DIR_TGT}/ffseq_${TIS}.txt"
+
+    # COMBINE IMAGES WITH TEXT FILE AS PLAYLIST
+    ffmpeg -r 30 -f concat -safe 0 -i "${DIR_TGT}/ffseq-${TIS}.txt" -c:v libx264 -pix_fmt yuv420p "${DIR_TGT}/sequence-${MARKER}-${TIC,,}.mp4"   
+    if [[ $(compgen -c | grep -iw 'ffmpeg' | head -n1 | wc -l) > "0" ]]; then    
+        celluloid ${DIR_TGT}/sequence-${MARKER}-${TIC,,}.mp4
+    fi    
+    rm -f "${DIR_TGT}/ffseq-${TIS}.txt"
 fi
