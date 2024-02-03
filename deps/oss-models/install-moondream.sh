83b23fc45e2884eff7c21bf6bbe1fb8af6bb7c4c
Lee Jordan <duracell80@users.noreply.github.com>
2024-02-03 15:01:37 -0600

    Updates

--- deps/oss-models/install-moondream.sh
+++ deps/oss-models/install-moondream.sh
@@ -9,12 +9,14 @@ BIN="${PTH}/${ENV}/bin"
 
 #sudo apt install lzma
 
-git clone https://github.com/vikhyat/moondream.git $NME && cd $NME
+git clone https://github.com/vikhyat/moondream.git $NME
+cd "${PTH}" && chmod +x "${PTH}/sample.py"
 
 python3.9 -m venv "${PTH}/${ENV}"
 source "${BIN}/activate" && mkdir -p "${APP}"
 
 pip install -r "${PTH}/requirements.txt"
+python3 "${PTH}/sample.py" --image="${CWD}/media/test.jpg" --prompt="describe this image"
 
+cp -r "${PTH}/moondream" "${APP}"
 cp "${PTH}/sample.py" "${APP}"
-chmod +x "${APP}/sample.py"
