f8e09deabf2d5cfa3cc30961459e15421abf0758
Lee Jordan <duracell80@users.noreply.github.com>
2024-02-04 00:34:15 -0600

    Updates

--- deps/oss-models/install-moondream.sh
+++ deps/oss-models/install-moondream.sh
@@ -6,36 +6,51 @@ ENV="${NME}-venv"
 PTH="${CWD}/${NME}"
 APP="${PTH}/${ENV}/app"
 BIN="${PTH}/${ENV}/bin"
+BIH="${HOME}/.local/bin"
+INS="${HOME}/.local/share/oss-models/${NME}"
+APH="${INS}/app"
+
 
 #sudo apt install lzma
 
 echo "[i] Installing Moondream from GIT"
 
-git clone https://github.com/vikhyat/moondream.git $NME
+if [ -d "${PTH}" ]; then
+	cd $NME
+	git fetch
+	git pull
+	cd ../
+else
+	git clone https://github.com/vikhyat/moondream.git $NME
+fi
 cd "${PTH}" && chmod +x "${PTH}/sample.py"
 
 echo "[i] Creating Python VENV"
 python3.9 -m venv "${PTH}/${ENV}"
 source "${BIN}/activate" && mkdir -p "${APP}"
 
+pip install wheel
 pip install -r "${PTH}/requirements.txt"
 
 echo "[i] Running a test description ..."
 python3 "${PTH}/sample.py" --image="${CWD}/media/test.jpg" --prompt="describe this image"
 
 cp -r "${PTH}/moondream" "${APP}"
-cp "${PTH}/sample.py" "${APP}/main.py"
-cp "${CWD}/scripts/${NME}-main.sh" "${APP}/main.sh"
-cp "${CWD}/scripts/${NME}-main.sh" "${HOME}/.local/bin/moondream"
+cp "${CWD}/scripts/moondream-main.py" "${APP}/main.py"
 
-chmod +x "${APP}/*.sh"
-chmod +x "${APP}/*.py"
 
 echo "[i] Moving Python VENV to local share folder"
 
 
 mkdir -p $HOME/.local/share/oss-models
-rm -rf "${HOME}/.local/share/oss-models"
-mv -f "${PTH}/${ENV}" "${HOME}/.local/share/oss-models/${NME}"
+rm -rf "${HOME}/.local/share/oss-models/${NME}"
+mv -f "${PTH}/${ENV}" "${INS}"
+
+cp "${CWD}/scripts/${NME}-main.sh" "${INS}/app/main.sh"
+cp "${CWD}/scripts/${NME}-main.sh" "${BIH}/${NME}"
+
+chmod +x "${APH}/main.sh"
+chmod +x "${APH}/main.py"
+
 
 echo "[i] Done!"
--- deps/oss-models/scripts/moondream-main.sh
+++ deps/oss-models/scripts/moondream-main.sh
@@ -2,5 +2,15 @@
 
 APP="${HOME}/.local/share/oss-models/moondream"
 source "${APP}/bin/activate"
+START=$(date +%s)
+
 bash -c "${APP}/bin/python3 ${APP}/app/main.py --cpu --image=${1} --prompt='${2}'"
+
+END=$(date +%s)
+TOTAL=$(( $END - $START ))
+
+RESPONSE=$(cat "${1}.txt")
+exiftool -overwrite_original -Exif:ImageDescription="${RESPONSE}" -Description="${RESPONSE}" "${1}"
+
+echo "Running time: ${TOTAL}s"
 exit
