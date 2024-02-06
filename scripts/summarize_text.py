c9f13d26c4cfec4fb347ac8cbc614b79d9cc60d0
Lee Jordan <duracell80@users.noreply.github.com>
2024-02-06 00:11:44 -0600

    Add action to summarize text with mistral in ollama

---
+++ scripts/summarize_text.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import sys, re, requests, json, time, argparse
+
+if len(sys.argv) < 4:
+	print("Usage: python summarize.py --model=mistral:latest --text=<path/to/file.txt> --words=1000 --prompt='summarize this text'")
+	sys.exit()
+
+def limit(s,n): return s[:n-1-(s+" ")[n-1::-1].find(" ")]
+
+def parse_arguments():
+	parser = argparse.ArgumentParser(description='Ollama text summarization')
+	parser.add_argument('-m', '--model', required=True, help='For example mistral:latest')
+	parser.add_argument('-t', '--text', required=True, help='Path to the text file')
+	parser.add_argument('-w', '--words', required=True, help='Read the first x words of the text file')
+	parser.add_argument('-p', '--prompt', default='summarize this text', help='Custom prompt for text analysis')
+
+	return parser.parse_args()
+
+args = parse_arguments()
+
+
+with open(args.text, 'r') as file:
+	string = file.read().replace('\n', '').replace('"', "'").replace(".", ". ")
+
+input = limit(string,1000)
+
+data = {
+  "prompt": f"{args.prompt}: {input}",
+  "model": f"{args.model}"
+}
+
+task_s = int(time.time())
+response = requests.post("http://localhost:11434/api/generate", json=data, stream=True)
+
+output = ""
+for line in response.iter_lines():
+	if line:
+		json_data = json.loads(line)
+		if json_data['done'] == False:
+			generated = json_data['response']
+			output += generated
+			print(generated, end='', flush=True)
+
+with open(f'{args.text}_summary.txt', 'w', encoding='utf-8') as f:
+	f.write(output)
+
+task_e = int(time.time())
+task_t = task_e - task_s
+
+
+
+print(f"\n\n[i] Task took: {task_t}s")
---
+++ scripts/summarize_text.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if ! [ -x "$(command -v ollama)" ]; then
+  echo 'Error: ollama is not installed.' >&2
+  zenity --error \
+          --text="Error: ollama is not installed."
+  exit 1
+fi
+
+
+FILE_DIR=$(dirname "$1")
+FILE_BIT=$(basename "$1")
+
+FILE_NME=${FILE_BIT%%.*}
+FILE_EXT=${FILE_BIT##*.}
+
+
+if [ "$FILE_EXT" = "txt" ] || [ "$FILE_EXT" = "log" ]; then
+    (
+    echo "50"
+    "${HOME}/.local/share/powertoys/summarize_text.py" --model=mistral:latest --text="${1}" --words=2000 --prompt="summarize this text"
+    echo "100";
+
+    ) |
+    zenity --progress \
+    --title="Text summary" \
+    --text="Summarizing text with mistral in ollama ..." \
+    --percentage=25 \
+    --width=500 \
+    --timeout=180
+else
+    zenity --error \
+          --text="Error: Supplied file is not a .txt or .log"
+fi
+
+zenity --text-info --title="Description of image" --filename="${1}_summary.txt"
+
