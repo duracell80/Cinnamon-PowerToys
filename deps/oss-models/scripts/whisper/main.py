c15702c49a9d4f759cc733f2e57b1f1a39998899
Lee Jordan <duracell80@users.noreply.github.com>
2024-02-08 00:44:11 -0600

    Updates

--- deps/oss-models/scripts/whisper/main.py
+++ deps/oss-models/scripts/whisper/main.py
@@ -1,28 +1,27 @@
 #!../bin/python3
 
-import argparse, time, pysubs2
+import argparse, pathlib, time, sys, pysubs2
 from faster_whisper import WhisperModel
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--audio", type=str, required=True)
+	parser.add_argument("--file", type=str, required=True)
 	parser.add_argument("--model", type=str, required=True)
 	parser.add_argument("--cpu", action="store_true")
 
 	args = parser.parse_args()
 
-
-
-
+	file_extension = pathlib.Path(args.file).suffix
 	model_size = f"{args.model}"
+
 	if args.cpu:
 		model = WhisperModel(model_size, device="cpu", compute_type="int8")
 	else:
 		model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
 	start_time = time.time()
-	segments, _ = model.transcribe(audio=f"{args.audio}", beam_size=5)
+	segments, _ = model.transcribe(audio=f"{args.file}", beam_size=5)
 
 	results = []
 	result = ""
@@ -43,14 +42,11 @@ if __name__ == "__main__":
 
 
 	subs = pysubs2.load_from_whisper(results)
-	if ".mp3" in args.audio:
-		subs.save(f"{str(args.audio).replace('.mp3', '.srt')}")
-		subs.save(f"{str(args.audio).replace('.mp3', '.ass')}")
-		transcription_file = str(args.audio).replace('.mp3', '.txt')
-	elif ".wav" in args.audio:
-		subs.save(f"{str(args.audio).replace('.wav', '.srt')}")
-		subs.save(f"{str(args.audio).replace('.wav', '.ass')}")
-		transcription_file = str(args.audio).replace('.wav', '.txt')
+
+	subs.save(f"{str(args.file).replace(file_extension, '.srt')}")
+	subs.save(f"{str(args.file).replace(file_extension, '.ass')}")
+
+	transcription_file = str(args.file).replace(file_extension, '.txt')
 
 	end_time = time.time()
 	total_time = round(end_time - start_time)
--- deps/oss-models/scripts/whisper/main.sh
+++ deps/oss-models/scripts/whisper/main.sh
@@ -2,10 +2,20 @@
 
 FILE_FUL=$(basename "${1}")
 FILE_DIR=$(dirname "${1}")
+FILE_EXT="${FILE_FUL##*.}"
 
-FILE_NME="${FILE_DIR}/${FILE_FUL%.*}"
+FILE_NME="${FILE_FUL%.*}"
+mkdir -p "${FILE_DIR}/.meta"
+
+ffmpeg -hide_banner -loglevel error -n -i "${1}" -b:a 192K -vn "${FILE_DIR}/.meta/${FILE_NME}_audio.mp3"
+
+if [ -f "${FILE_DIR}/.meta/${FILE_NME}_audio.mp3" ]; then
+	./main.py --cpu --model=small.en --file="${FILE_DIR}/.meta/${FILE_NME}_audio.mp3"
+fi
 
 
-./main.py --cpu --model=small.en --audio="${1}"
-ffmpeg -y -i "${1}" -i "${FILE_NME}.srt" -i "${FILE_NME}.jpg" -c:v libx264 -pix_fmt yuv420p -vf scale=1280:720 -c:s mov_text -c:a copy "${FILE_NME}.mp4"
-
+if [[ $FILE_EXT == *"mp3"* ]] || [[ $FILE_EXT == *"wav"* ]]; then
+	ffmpeg -hide_banner -loglevel error -y -i "${1}" -i "${FILE_DIR}/.meta/${FILE_NME}_audio.srt" -i "${FILE_DIR}/${FILE_NME}.jpg" -c:v libx264 -pix_fmt yuv420p -vf scale=1280:720 -c:s mov_text -c:a copy "${FILE_DIR}/${FILE_NME}_captioned.mp4"
+elif [[ $FILE_EXT == *"mp4"* ]] || [[ $FILE_EXT == *"mkv"* ]]; then
+	ffmpeg -hide_banner -loglevel error -y -i "${1}" -i "${FILE_DIR}/.meta/${FILE_NME}_audio.srt" -c:s mov_text -c:v copy -c:a copy "${FILE_DIR}/${FILE_NME}_captioned.mp4"
+fi
