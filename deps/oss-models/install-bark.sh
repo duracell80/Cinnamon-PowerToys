8c3179a6163143865ace511e22b606e758ec3c02
Lee Jordan <duracell80@users.noreply.github.com>
2024-02-15 21:44:41 -0600

    Add Bark to local models

---
+++ deps/oss-models/install-bark.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+CWD=$(pwd)
+NME="bark"
+ENV="${NME}-venv"
+PTH="${CWD}/${NME}"
+APP="${PTH}/${ENV}/app"
+BIN="${PTH}/${ENV}/bin"
+BIH="${HOME}/.local/bin"
+INS="${HOME}/.local/share/oss-models/${NME}"
+APH="${INS}/app"
+
+#sudo apt install lzma
+
+echo "[i] Installing ${NME} from GIT"
+
+if [ -d "${PTH}" ]; then
+	cd $NME
+	git fetch
+	git pull
+	cd ../
+else
+	git clone https://github.com/suno-ai/bark $NME
+fi
+cd "${PTH}" && chmod +x "${PTH}/setup.py"
+
+echo "[i] Creating Python VENV"
+python3.10 -m venv "${PTH}/${ENV}"
+source "${BIN}/activate" && mkdir -p "${APP}/media"
+
+pip install wheel scipy
+pip install .
+#pip install -r "${PTH}/requirements.txt"
+
+#cp -r "${PTH}/bark" "${APP}"
+
+cp -f "${CWD}/scripts/bark/bark-test.py" "${APP}/bark-test.py"
+#cp -f "${CWD}/scripts/whisper/main.py" "${APP}/main.py"
+#cp -f "${CWD}/scripts/whisper/main.sh" "${APP}/main.sh"
+echo "[i] Running a test transformation ..."
+python3 "${APP}/bark-test.py"
+play "${APP}/media/bark_test.wav"
+
+mkdir -p $HOME/.local/share/oss-models
+rm -rf "${HOME}/.local/share/oss-models/${NME}"
+mv -f "${PTH}/${ENV}" "${INS}"
+
+#cp -f "${CWD}/scripts/${NME}/main.sh" "${APH}/main.sh"
+cp -f "${CWD}/scripts/${NME}/main.py" "${APH}/main.py"
+
+#chmod +x "${APH}/main.sh"
+chmod +x "${APH}/main.py"
+
+#cp "${CWD}/scripts/${NME}/main.sh" "${BIH}/${NME}"
+#chmod +x "${BIH}/${NME}"
+
+#cp -f "${CWD}/../../nemo/actions/pt-caption-media.nemo_action" "${HOME}/.local/share/nemo/actions"
+#cp -f "${CWD}/../../scripts/caption_mediafiles.sh" "${HOME}/.local/share/powertoys"
+
+
+
+echo "[i] Done!"
---
+++ deps/oss-models/scripts/bark/main.py
@@ -0,0 +1,19 @@
+#!../bin/python3
+
+import scipy, time
+from transformers import AutoProcessor, BarkModel
+
+ts = int(time.time())
+
+processor = AutoProcessor.from_pretrained("suno/bark")
+model = BarkModel.from_pretrained("suno/bark")
+
+voice_preset = "v2/en_speaker_6"
+
+inputs = processor("Severe thunderstorm warning in effect for Davidson County until 4 29 pm", voice_preset=voice_preset)
+
+audio_array = model.generate(**inputs)
+audio_array = audio_array.cpu().numpy().squeeze()
+
+sample_rate = model.generation_config.sample_rate
+scipy.io.wavfile.write(f"media/output_{str(ts)}.wav", rate=sample_rate, data=audio_array)
