b1cb87343d9954afbc94a9fd4c85fb608362f8a6
Lee Jordan <duracell80@users.noreply.github.com>
2024-06-05 20:30:53 -0500

    Update spice

--- spices/actions/cinnamon-take-screenshot@duracell80/files/cinnamon-take-screenshot@duracell80/icon-generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# eg $1=Mint-Y/places/symbolic/folder-pictures-symbolic.svg
-
-inkscape --export-width=128 --export-type=png --export-background-opacity=0 --export-filename=icon.png "/usr/share/icons/${1}"
